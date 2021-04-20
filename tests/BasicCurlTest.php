4e80496d87ac6f8c483fbb280b26901800d7a995
Atif Nazir <atifn@atifn.net>
2021-04-20 02:59:23 +0000

    indentation

--- tests/BasicCurlTest.php
+++ tests/BasicCurlTest.php
@@ -5,24 +5,24 @@ use PHPUnit\Framework\TestCase;
 class BasicCurlTest extends TestCase
 {
     private $blockio;
-
+    
     protected function setUp(): void {
-	parent::setUp();
-	$this->blockio = new \BlockIo\Client("", "", 2);
+        parent::setUp();
+        $this->blockio = new \BlockIo\Client("", "", 2);
     }
-
+    
     protected function tearDown(): void {
-	parent::tearDown();
+        parent::tearDown();
     }
-
+    
     public function testBadAPIKey()
     { // check that we're getting the appropriate data from Block.io via cURL
-
-	$this->expectException(\Exception::class);
-	$this->expectExceptionMessage("Failed: Invalid API Key provided for this API version.");
-
-	$this->blockio->get_balance();
-	
+        
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage("Failed: Invalid API Key provided for this API version.");
+        
+        $this->blockio->get_balance();
+        
     }
     
 }
--- tests/BasicWithWIFTest.php
+++ tests/BasicWithWIFTest.php
@@ -9,22 +9,22 @@ class BasicWithWIFTest extends TestCase
     private $key;
     
     protected function setUp(): void {
-	parent::setUp();
-	$this->blockio = new \BlockIo\Client("", "", 2);
-	$this->data_to_sign = "e76f0f78b7e7474f04cc14ad1343e4cc28f450399a79457d1240511a054afd63";
-	$this->key = $this->blockio->initKey()->fromWIF("L1cq4uDmSKMiViT4DuR8jqJv8AiiSZ9VeJr82yau5nfVQYaAgDdr");
+        parent::setUp();
+        $this->blockio = new \BlockIo\Client("", "", 2);
+        $this->data_to_sign = "e76f0f78b7e7474f04cc14ad1343e4cc28f450399a79457d1240511a054afd63";
+        $this->key = $this->blockio->initKey()->fromWIF("L1cq4uDmSKMiViT4DuR8jqJv8AiiSZ9VeJr82yau5nfVQYaAgDdr");
     }
-
+    
     protected function tearDown(): void {
-	parent::tearDown();
+        parent::tearDown();
     }
-
+    
     public function testKeyFromWIF()
     {
-	$this->assertEquals($this->key->getPublicKey(), "024988bae7e0ade83cb1b6eb0fd81e6161f6657ad5dd91d216fbeab22aea3b61a0");
-	$this->assertEquals($this->key->signHash($this->data_to_sign), "3044022061753424b6936ca4cfcc81b883dab55f16d84d3eaf9d5da77c1e25f54fda963802200d3db78e8f5aac62909c2a89ab1b2b413c00c0860926e824f37a19fa140c79f4");
+        $this->assertEquals($this->key->getPublicKey(), "024988bae7e0ade83cb1b6eb0fd81e6161f6657ad5dd91d216fbeab22aea3b61a0");
+        $this->assertEquals($this->key->signHash($this->data_to_sign), "3044022061753424b6936ca4cfcc81b883dab55f16d84d3eaf9d5da77c1e25f54fda963802200d3db78e8f5aac62909c2a89ab1b2b413c00c0860926e824f37a19fa140c79f4");
     }
-
+    
 }
 
 ?>
