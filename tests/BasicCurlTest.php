0ff8cd6cebc2ae05939bd584651428c3477a45e7
Atif Nazir <atifn@atifn.net>
2021-06-01 04:09:02 +0000

    use https:// for curl.haxx.se link + update test error

--- tests/BasicCurlTest.php
+++ tests/BasicCurlTest.php
@@ -18,7 +18,7 @@ class BasicCurlTest extends TestCase
     public function testBadAPIKey()
     { // check that we're getting the appropriate data from Block.io via cURL
         
-        $expected_response = json_decode('{"status":"fail","data":{"error_message":"Invalid API Key provided for this API version."}}');
+        $expected_response = json_decode('{"status":"fail","data":{"error_message":"API Key invalid or you have not enabled API access for this machine\'s IP address(es). Check that your API Keys are correct, and that you have enabled API access for this machine\'s IP Address(es) on your account\'s Settings page."}}');
 
         try {
             $response = $this->blockio->get_balance();
